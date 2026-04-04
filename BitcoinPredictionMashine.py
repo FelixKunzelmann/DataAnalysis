56d9078591d1c58b6243f88ebf2119f6d837b60b
Felix <felix.kunzelmann@gmx.de>
2026-04-04 19:21:09 +0200

    Added plot to Bitcoin predictor

--- BitcoinPredictionMashine.py
+++ BitcoinPredictionMashine.py
@@ -47,7 +47,7 @@ model.add(Dense(1))
 model.compile(optimizer='adam', loss='mean_squared_error')
 
 # Train the model
-model.fit(X_train, y_train, epochs=10, batch_size=32, verbose=1)
+model.fit(X_train, y_train, epochs=40, batch_size=32, verbose=1)
 
 # Predict on test data
 predictions = model.predict(X_test)
@@ -58,15 +58,6 @@ y_test_actual = scaler.inverse_transform(y_test.reshape(-1, 1))
 rmse = np.sqrt(mean_squared_error(y_test_actual, predictions))
 print(f'RMSE: {rmse}')
 
-# Plot predictions vs actual
-plt.figure(figsize=(14, 5))
-plt.plot(y_test_actual, color='blue', label='Actual Bitcoin Price')
-plt.plot(predictions, color='red', label='Predicted Bitcoin Price')
-plt.title('Bitcoin Price Prediction')
-plt.xlabel('Time')
-plt.ylabel('Price')
-plt.legend()
-plt.show()
 
 # Predict future days (next 30 days)
 last_60_days = scaled_data[-60:]
@@ -82,3 +73,37 @@ future_predictions = scaler.inverse_transform(
 print('Predicted prices for next 30 days:')
 for i, price in enumerate(future_predictions):
     print(f'Day {i+1}: {price[0]}')
+
+# Plot predictions vs actual
+plt.figure(figsize=(14, 5))
+plt.plot(y_test_actual, color='blue', label='Actual Bitcoin Price')
+plt.plot(predictions, color='red', label='Predicted Bitcoin Price')
+plt.title('Bitcoin Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+plt.show()
+
+# Second plot: Last 60 days actual + last 30 days predicted + next 30 days predicted
+plt.figure(figsize=(14, 5))
+last_60_actual = data[-60:].values.flatten()
+future_dates = pd.date_range(
+    start=data.index[-1] + pd.Timedelta(days=1), periods=30)
+
+# Get dates and predictions for the last 30 days of test data
+test_start_idx = train_size + time_step
+test_dates = data.index[test_start_idx: test_start_idx + len(predictions)]
+last_30_test_dates = test_dates[-30:]
+last_30_predictions = predictions[-30:]
+
+plt.plot(data.index[-60:], last_60_actual,
+         color='blue', label='Last 60 Days Actual')
+plt.plot(last_30_test_dates, last_30_predictions.flatten(),
+         color='green', label='Predicted (Last 30 Test Days)')
+plt.plot(future_dates, future_predictions.flatten(),
+         color='red', label='Next 30 Days Predicted')
+plt.title('Bitcoin Price: Last 60 Days Actual + Predictions')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend()
+plt.show()
